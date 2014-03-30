c701dfc04bb5dbb66826dafe923efb9a5103b99d
newsky1006 <newsky1006@e649eed1-7011-89d2-eede-ad707452cd20>
2014-03-30 14:29:43 +0000

    dlu helper

---
+++ trunk/TKBThucHanh/DluWebHelper/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DluWebHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DluWebRequest webRequest  =new DluWebRequest();
+            webRequest.GetCurentTimeTable();
+        }
+    }
+}
